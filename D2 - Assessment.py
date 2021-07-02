af2260b3005bb6f4f87dd76a57db8da3662f7f23
DaijaStokes <77857472+DaijaStokes@users.noreply.github.com>
2021-07-02 15:04:57 -0400

    practice_03

---
+++ D2 - Assessment.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+#Question 1:
+# Open the file banking_data_for_Dict without pandas and print the no of lines of data
+  
+    
+    
+#Question 2:
+# Write a Python script which will print 3rd and 5th column of every row
+
+     
+# Question 3:
+# Write a Python function which will print 2nd column for even rows, 3rd colum for odd rows
