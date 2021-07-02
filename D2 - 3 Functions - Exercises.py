477cee549d15783ed2f5443db0eec6b56bfe047e
DaijaStokes <77857472+DaijaStokes@users.noreply.github.com>
2021-07-02 15:06:35 -0400

    practice

---
+++ D2 - 3 Functions - Exercises.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# 1 
+#Create a function to calculate the absolute value of a number without using the abs() function to calculate the absolute value for -9 and 7.2
+
+
+# 2
+# for the given data (20,30,40,50,100), create a function to calcuate the total/3 
+#note that all the calcualtions have to be done within the function
+
+
+# 3
+#create a function to work in range(43,60) such that if the number is <50, it prints out "LT_50" else it should print out (number+3)
+
+
+#4
+# create a functio which takes 2 inputs as num and x and do the following
+#if num>=0 then calcuate num*x else num*x/2
+
+
+#5 create a function to create a new list with only the nos which can be divided by 3 from the following list
+li = [5, 7, 21, 99, 51, 62, 77, 27, 73, 61] 
+
+
+# 6
+#create a function using lambda function to overwrite the following list with all the elements squared
+li = [5, 7, 22, 97, 54, 62, 77, 23, 73, 61] 
+
